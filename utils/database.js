fe65f1a1a7577d86f2a0374210485e138eb0ebb6
allwintitus <71762131006@cit.edu.in>
2024-09-07 10:16:43 +0530

    first commit

---
+++ utils/database.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose'
+
+let isConnected=false;//track the connection
+
+export const connectToDB=async()=>{
+      mongoose.set('strictQuery',true);
+
+      if(isConnected){
+        console.log("Mongo db is Already Connected");
+        return;
+      }
+      try{
+        await mongoose.connect(process.env.MONGODB_URI,{
+             dbName:"share_prompt",
+             useNewUrlParser:true,
+             useUnifiedTopology:true,
+        })
+       isConnected=true;
+       
+       console.log('Mongo Db Connected')
+      }catch(error){
+     console.log(error)
+      }
+}
