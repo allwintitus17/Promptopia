b0767e0eff1b36793f9dc3a8d9b212aa1fb105da
allwintitus <71762131006@cit.edu.in>
2024-09-09 12:18:40 +0530

    api route for users changed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -18,7 +18,7 @@ const handler=NextAuth({
              email: session.user.email
             })
      
-            session.user.id=sessionUser._id.toString();
+            session.user.id=sessionUser.id.toString();
      
             return session;
          }, 
